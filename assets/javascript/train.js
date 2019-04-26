1b2ae5b02c7cd980f5a47e327edf14d55d4f279e
Isabella Merendino <isabellamerendino@Isabellas-MacBook-Pro.local>
2019-04-26 19:24:06 -0400

    added HTML/CSS

---
+++ assets/javascript/train.js
@@ -0,0 +1,32 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDfwR1np1MKFHr8_fXGGEscMYmBfUQ41Ks",
+    authDomain: "trainactivity-c7feb.firebaseapp.com",
+    databaseURL: "https://trainactivity-c7feb.firebaseio.com",
+    projectId: "trainactivity-c7feb",
+    storageBucket: "trainactivity-c7feb.appspot.com",
+    messagingSenderId: "9182396337"
+};
+firebase.initializeApp(config);
+
+var dataRef = firebase.database();
+
+// Capture Button Click
+$("#addTrain").on("click", function (event) {
+    event.preventDefault();
+
+    var newTrip = {
+        name: $("#trainName").val().trim(),
+        destination: $("#inputDestination").val().trim(),
+        time: $("#firstTrainTime").val().trim(),
+        frequency: ("#trainFrequency").val().trim()
+    }
+
+    $("#trainName").val("");
+    $("#inputDestination").val("");
+    $("#firstTrainTime").val("");
+    $("#trainFrequency").val("");
+
+// Code for the push
+    dataRef.ref().push({newTrip});
+});
